e8712d20a3de6aab0638def1b8a95ecd3e1aae26
zvm23 <zhenyuch@usc.edu>
2025-04-02 21:16:32 -0700

    Add a job position scraping script.
    Defined few functions such as parse_search_page, get_job_positions, etc.

---
+++ scripts/data scraping/jobPos_scraper.py
@@ -0,0 +1,43 @@
+﻿import os
+import re
+import json
+import math
+import urllib
+
+from typing import Dict, List
+from loguru import logger as log
+from scrapfly import ScrapeApiResponse, ScrapeConfig, ScrapflyClient, ScrapflyScrapeError
+
+
+def parse_search_page(result):
+    """
+    Find hidden web data of search results in Indeed.com search page HTML.
+    """
+    data = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', result.content)
+    data = json.loads(data[0])
+    search_page = {
+        "results": data["metaData"]["mosaicProviderJobCards"]["results"],
+        "meta": data["metaData"]["mosaicProviderJobCardsModel"]["tierSummaries"],
+    }
+    return search_page
+
+
+def get_job_positions(client: ScrapflyClient, url: str) -> str:
+    r = client.scrape(ScrapeConfig(url=url, **BASE_CONFIG))
+    return r
+
+
+def main():
+    url_indeed = "https://www.indeed.com/jobs?q=data%20science&l="
+    get_job_positions(SCRAPFLY, url_indeed)
+
+
+if __name__ == "__main__":
+    SCRAPFLY = ScrapflyClient(key=os.environ["SCRAPFLY_API_KEY"])
+    BASE_CONFIG = {
+        "asp": True,
+        "country": "US",
+    }
+    
+    main()
+
