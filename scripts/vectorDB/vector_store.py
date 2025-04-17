a0fcb2a049a8b7ec8f4331533fe4bfdc21a71be0
zvm23 <zhenyuch@usc.edu>
2025-04-17 00:00:19 -0700

    Update load data function.

--- scripts/vectorDB/vector_store.py
+++ scripts/vectorDB/vector_store.py
@@ -1,4 +1,5 @@
 ﻿import os
+import json
 import getpass
 import numpy as np
 
@@ -14,7 +15,6 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
 
 
-#* Function for loading a list of APIs from the environment
 def load_api():
     apiKeys = ["OPENAI_API_KEY", "OLLAMA_API_KEY", "ANTHROPIC_API_KEY", 
             "HUGGINGFACE_API_KEY", "GEMINI_API_KEY", "CLAUDE_API_KEY"]
@@ -23,22 +23,29 @@ def load_api():
             os.environ[api] = getpass.getpass(f"Please enter your {api}: ")
 
 
-#* Function for loading data
-def load_data(docs):
-    text = ""
+def load_data(docs: list[str]) -> list[str]:
+    """Return a list of the documents' text content"""
+    text_list = []
     for doc in docs:
         if doc.endswith(".pdf"):
-            loader = PyMuPDFLoader(doc)
+            text = ""
+            loader = PyMuPDFLoader(doc, extract_tables=True)
+            
             for page in loader.load():
                 text += page.page_content
+            text_list.append(text)
+        
         elif doc.endswith(".json"):
+            # TODO: If the document is a json file, apply Document Augmentation using llm
+            text = ""
             loader = JSONLoader(doc)
             for item in loader.load():
                 text += item.page_content
-    return text
+            text_list.append(text)
+    
+    return text_list
 
 
-#* Function for chunking text
 def chunk_text(text):
     chunker = TokenChunker(
         tokenizer = "character",
@@ -51,26 +58,25 @@ def chunk_text(text):
     return chunks
 
 
-#* Function that defines the vector store of the Qdrant Database
 def create_vectorStore(emb_model):
     #client = QdrantClient(":memory:")
     client = QdrantClient(url="http://localhost:6333")
     
     client.create_collection(
-        collection_name="job_collection",
+        collection_name="DS_jobs",
         vectors_config=VectorParams(size=1024, distance=Distance.COSINE),
     )
     
     if emb_model == "text-embedding-3-large":
         vector_store = QdrantVectorStore(
             client=client,
-            collection_name="job_collection",
+            collection_name="DS_jobs",
             embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
         )
     elif emb_model == "nomic-embed-text":
         vector_store = QdrantVectorStore(
             client=client,
-            collection_name="job_collection",
+            collection_name="DS_jobs",
             embedding_function=ollama.embed(model="nomic-embed-text"),
         )
     
