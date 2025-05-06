d47a9efb1b8f83d4bb3a647f58c82f9ebb98cba5
Yiwen23 <55377411+YiwenC23@users.noreply.github.com>
2025-05-06 02:17:05 -0700

    Changed name of the main script.

---
+++ scripts/app.py
@@ -0,0 +1,6 @@
+﻿import streamlit as st
+
+pg_main = st.Page("web/cvec.py", title="Career Vector", icon="🏠", default=True)
+pg_chatbot = st.Page("web/resume_q&a.py", title="Resume Polish", icon=":material/edit:")
+pg = st.navigation([pg_main, pg_chatbot])
+pg.run()
